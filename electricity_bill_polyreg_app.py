0474ff3f579ef37750e3728d7ebe94c2cca947a3
Michel <149605902+githubiamhere@users.noreply.github.com>
2026-09-18 09:34:46 +0530

    Implement electricity bill prediction app with Streamlit
    Added a Streamlit app for predicting electricity bills based on AC units input.

---
+++ electricity_bill_polyreg_app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+model = joblib.load("electricity_bill_prediction_model.pkl")
+poly = joblib.load("poly.pkl")
+st.title("Electrical_Bill AC Price Prediction")
+
+units = st.number_input(
+    "Enter AC Units : ",
+    min_value=0.0,
+    max_value=150.0
+    value=100.0
+)
+
+
+if st.button("Predict"):
+        input_data = pd.DataFrame({
+            "AC_Units": [units],
+        })
+        input_data_poly = poly.transform(input_data)
+        prediction = model.predict(input_data_poly)
+        pred = prediction[0]
+        st.success(f"Predicted Price: ₹{pred:.2f} Rs")
+import joblib
+joblib.dump(model,"electricity_bill_prediction_model.pkl")
+joblib.dump(poly, "poly.pkl")
